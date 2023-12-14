8ea8e257c1d9a8b01dd0977bb48a9f44591a0df9
sofia <sofia.jimlap@educa.jcyl.es>
2023-12-14 18:31:28 +0100

    ejercicio clientes 1

--- src/app/componentes/cliente-alta/cliente-alta.component.ts
+++ src/app/componentes/cliente-alta/cliente-alta.component.ts
@@ -18,20 +18,25 @@ import {NgForOf} from "@angular/common";
 export class ClienteAltaComponent implements OnInit{
 
   nombre = "";
-  cif = ""
-  direccion = ""
-  grupo = 0
-  clientes : Cliente[] = []
-  grupos : Grupo[] = []
+  cif = "";
+  direccion = "";
+  grupo = 0;
+  grupos : Grupo[] = [];
 
   constructor(private clienteService : ClientesService) {}
 
   guardar (){
     this.clienteService.addClientes({id: 1, nombre: this.nombre, cif: this.cif, direccion: this.direccion, grupo : this.grupo})
+    this.clear()
+  }
+  clear (){
+    this.nombre = "";
+    this.cif = "";
+    this.direccion = "";
+    this.grupo = 0;
   }
 
   ngOnInit(){
-    this.clientes = this.clienteService.getClientes()
     this.grupos = this.clienteService.getGrupos()
   }
 

fad11c27276ff0cad28551826a854fc5d1b372ca
sofia <sofia.jimlap@educa.jcyl.es>
2023-12-19 19:44:53 +0100

    ejemplo simple observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,8 +26,8 @@ export class AppComponent implements OnInit {
   // Para indicar que la variable es Observable
   //2.	Averigua en que consiste el pipe | async
   /*
-    La asynctubería se suscribe a un Observableo Promisey devuelve el último
-     valor que ha emitido. Cuando se emite un nuevo valor, la asynctubería
+    La async tubería se suscribe a un Observableo Promise y devuelve el último
+     valor que ha emitido. Cuando se emite un nuevo valor, la async tubería
      marca el componente que se verificará en busca de cambios. Cuando el
      componente se destruye, la asynccanalización se da de baja automáticamente
       para evitar posibles pérdidas de memoria.
